91dc33b2e440f37bd86d4fc63eec0f69565f5c27
Kevin Viana <vianakevinc2@hotmail.com>
2022-06-06 17:59:15 -0300

    RoutingModule para o módulo de Pessoas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PaginaNaoEncontradaComponent } from "./core/pagina-nao-encontrada.component";
-import { PessoaCadastroComponent } from "./pessoas/pessoa-cadastro/pessoa-cadastro.component";
-import { PessoasPesquisaComponent } from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'pessoa/novo', component: PessoaCadastroComponent },
-  { path: 'pessoa/:codigo', component: PessoaCadastroComponent },
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
   { path: '**', redirectTo: 'pagina-nao-encontrada' },
 ];
---
+++ src/app/pessoas/pessoas-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+
+const routes: Routes = [
+  { path: 'pessoas', component: PessoasPesquisaComponent },
+  { path: 'pessoa/novo', component: PessoaCadastroComponent },
+  { path: 'pessoa/:codigo', component: PessoaCadastroComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
