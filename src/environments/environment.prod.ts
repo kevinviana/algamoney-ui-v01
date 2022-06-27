9747d3383ee921757ec77833b7e49ff4a2c4516a
Kevin Viana <vianakevinc2@hotmail.com>
2022-06-27 14:41:23 -0300

    bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api-v01.herokuapp.com ',
+  apiUrl: 'https://algamoney-api-v01.herokuapp.com',
   tokenAllowedDomains: [/algamoney-api.herokuapp.com/],
   tokenDisallowedRoutes: [/\/oauth\/token/],
 };
